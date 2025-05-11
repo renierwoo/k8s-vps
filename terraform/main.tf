706eb1ee6ad4ac18183277f8fa61066ec162d97a
Renier Collazo Woo <rcollazo@emergya.com>
2025-05-11 03:21:07 +0200

    Update Terraform code for module vps-setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 module "vps_setup" {
-  source = "./modules/vps_setup"
+  source = "./modules/vps-setup"
 
-  connection_user        = var.connection_type
+  connection_user        = var.connection_user
   connection_host        = var.connection_host
   connection_port        = var.connection_port
   connection_private_key = var.connection_private_key
