587ae7ed84264a84688d51dfd682f12e85a38784
Renier Collazo Woo <rcollazo@emergya.com>
2025-05-12 01:37:25 +0200

    Update GitHub action for Terraform Plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,3 +37,16 @@ resource "aws_security_group_rule" "example" {
   security_group_id        = aws_security_group.example.id
   source_security_group_id = aws_security_group.example.id
 }
+
+resource "digitalocean_kubernetes_cluster" "demo" {
+  name   = "demo"
+  region = "lon1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 3
+  }
+}
