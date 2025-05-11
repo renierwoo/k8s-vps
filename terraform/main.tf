e8fdd10c915acda50e528696be3f9b9e5872e86a
Renier Collazo Woo <rcollazo@emergya.com>
2025-05-12 01:25:07 +0200

    Update GitHub action for Terraform Plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,29 @@ module "s3_bucket" {
 
   bucket = "my-s3-bucket"
 }
+
+locals {
+  rules = {
+    first = {
+      port = 1000
+      type = "ingress"
+    },
+    second = {
+      port = 1000
+      type = "egress"
+    }
+  }
+}
+
+#trivy:ignore:aws-ec2-no-public-ingress-sgr[from_port=1000,type=egress]
+resource "aws_security_group_rule" "example" {
+  for_each = { for k, v in local.rules : k => v }
+
+  type                     = each.value.type
+  from_port                = each.value.port
+  to_port                  = each.value.port
+  protocol                 = "TCP"
+  cidr_blocks              = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.example.id
+  source_security_group_id = aws_security_group.example.id
+}
