8946b7a66da7f9908608763f3acc16b93504f2d2
Renier Collazo Woo <rcollazo@emergya.com>
2025-06-05 01:51:27 +0200

    Update GitHub action for Terraform Plan

--- terraform/modules/vps-setup/main.tf
+++ terraform/modules/vps-setup/main.tf
@@ -28,3 +28,17 @@ resource "terraform_data" "update_os_packages" {
     private_key = var.connection_private_key
   }
 }
+
+
+resource "digitalocean_kubernetes_cluster" "demo" {
+  name   = "demo"
+  region = "lon1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 3
+  }
+}
