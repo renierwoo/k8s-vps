e0e5f6497e8365466a92218f5c467d93c44674b7
Renier Collazo Woo <rcollazo@emergya.com>
2025-06-05 02:14:04 +0200

    Update GitHub action for Terraform Plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,83 +7,83 @@ module "vps_setup" {
   connection_private_key = var.connection_private_key
 }
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+# module "s3_bucket" {
+#   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
-}
+#   bucket = "my-s3-bucket"
+# }
 
-locals {
-  rules = {
-    first = {
-      port = 1000
-      type = "ingress"
-    },
-    second = {
-      port = 1000
-      type = "egress"
-    }
-  }
-}
+# locals {
+#   rules = {
+#     first = {
+#       port = 1000
+#       type = "ingress"
+#     },
+#     second = {
+#       port = 1000
+#       type = "egress"
+#     }
+#   }
+# }
 
-resource "aws_security_group_rule" "example" {
-  for_each = { for k, v in local.rules : k => v }
+# resource "aws_security_group_rule" "example" {
+#   for_each = { for k, v in local.rules : k => v }
 
-  type                     = each.value.type
-  from_port                = each.value.port
-  to_port                  = each.value.port
-  protocol                 = "TCP"
-  cidr_blocks              = ["0.0.0.0/0"]
-  security_group_id        = aws_security_group.example.id
-  source_security_group_id = aws_security_group.example.id
-}
+#   type                     = each.value.type
+#   from_port                = each.value.port
+#   to_port                  = each.value.port
+#   protocol                 = "TCP"
+#   cidr_blocks              = ["0.0.0.0/0"]
+#   security_group_id        = aws_security_group.example.id
+#   source_security_group_id = aws_security_group.example.id
+# }
 
-resource "digitalocean_kubernetes_cluster" "demo" {
-  name   = "demo"
-  region = "lon1"
-  # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.24.4-do.0"
+# resource "digitalocean_kubernetes_cluster" "demo" {
+#   name   = "demo"
+#   region = "lon1"
+#   # Grab the latest version slug from `doctl kubernetes options versions`
+#   version = "1.24.4-do.0"
 
-  node_pool {
-    name       = "worker-pool"
-    size       = "s-2vcpu-2gb"
-    node_count = 3
-  }
-}
+#   node_pool {
+#     name       = "worker-pool"
+#     size       = "s-2vcpu-2gb"
+#     node_count = 3
+#   }
+# }
 
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-resource "aws_instance" "insecure_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
-  instance_type = "t2.micro"
+# resource "aws_instance" "insecure_ec2" {
+#   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+#   instance_type = "t2.micro"
 
-  associate_public_ip_address = true
+#   associate_public_ip_address = true
 
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
+#   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
-  tags = {
-    Name = "InsecureInstance"
-  }
-}
+#   tags = {
+#     Name = "InsecureInstance"
+#   }
+# }
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id      = "vpc-12345678" # Usa una VPC válida o usa data source
+# resource "aws_security_group" "allow_all" {
+#   name        = "allow_all"
+#   description = "Allow all inbound traffic"
+#   vpc_id      = "vpc-12345678" # Usa una VPC válida o usa data source
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
