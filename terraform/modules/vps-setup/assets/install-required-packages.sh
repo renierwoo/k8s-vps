52adfbee7c68dc2ccfbbeefc9086daf58d28d91d
Renier Collazo Woo <rcollazo@emergya.com>
2025-07-05 18:16:44 +0200

    Update GitHub action for Terraform Plan

--- terraform/modules/vps-setup/assets/install-required-packages.sh
+++ terraform/modules/vps-setup/assets/install-required-packages.sh
@@ -9,18 +9,20 @@ fi
 
 if command -v apt-get >/dev/null 2>&1; then
     echo "Debian/Ubuntu OS detected."
-    echo "Updating OS and system packages..."
+    echo "Installing required packages..."
 
     export DEBIAN_FRONTEND=noninteractive
 
     apt-get --quiet=2 --assume-yes update
-    apt-get -o Dpkg::Options::="--force-confold" --quiet=2 --assume-yes upgrade
-    apt-get -o Dpkg::Options::="--force-confold" --quiet=2 --assume-yes full-upgrade
+    apt-get -o Dpkg::Options::="--force-confold" --quiet=2 --assume-yes --no-install-recommends --no-install-suggests install \
+        htop \
+        vim \
+        git
     apt-get --quiet=2 --assume-yes autoremove
     apt-get --quiet=2 --assume-yes autoclean
     rm --recursive --force /tmp/* /var/tmp/* /var/cache/apt/* /var/lib/apt/lists/*
 
-    echo "OS and system packages updated successfully."
+    echo "Required packages installed successfully."
 
 else
     echo "Unsupported OS detected. Please run this script on a Debian/Ubuntu-based system."
