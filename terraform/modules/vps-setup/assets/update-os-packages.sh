6026f1c2f2ffeeea0f16b72c2302e98a8a8c7b63
Renier Collazo Woo <rcollazo@emergya.com>
2025-07-05 16:37:29 +0200

    Update GitHub action for Terraform Plan

--- terraform/modules/vps-setup/assets/update-os-packages.sh
+++ terraform/modules/vps-setup/assets/update-os-packages.sh
@@ -7,12 +7,17 @@ if ! sudo -n true 2>/dev/null; then
     exit 1
 fi
 
+export DEBIAN_FRONTEND=noninteractive
+
 if command -v apt-get >/dev/null 2>&1; then
     echo "Debian/Ubuntu OS detected."
     echo "Updating OS and system packages..."
+
+    export DEBIAN_FRONTEND=noninteractive
+
     apt-get --quiet=2 --assume-yes update
-    apt-get --quiet=2 --assume-yes upgrade
-    apt-get --quiet=2 --assume-yes full-upgrade
+    apt-get -o Dpkg::Options::="--force-confold" --quiet=2 --assume-yes upgrade
+    apt-get -o Dpkg::Options::="--force-confold" --quiet=2 --assume-yes full-upgrade
     apt-get --quiet=2 --assume-yes autoremove
     apt-get --quiet=2 --assume-yes autoclean
     rm --recursive --force /tmp/* /var/tmp/* /var/cache/apt/* /var/lib/apt/lists/*
