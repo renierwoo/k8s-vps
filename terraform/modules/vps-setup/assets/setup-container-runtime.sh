f17aad5e6c8c090de4ae08c98402345e53820dc6
Renier Collazo Woo <rcollazo@emergya.com>
2025-07-12 00:54:43 +0200

    Update GitHub action for Terraform Plan

--- terraform/modules/vps-setup/assets/setup-container-runtime.sh
+++ terraform/modules/vps-setup/assets/setup-container-runtime.sh
@@ -8,17 +8,28 @@ if ! sudo -n true 2>/dev/null; then
 fi
 
 REQUIRED_PACKAGES=(
-    htop
-    git
-    gnupg
-    vim
+    docker-ce
+    docker-ce-cli
+    containerd.io
+    docker-buildx-plugin
+    docker-compose-plugin
 )
 
 if command -v apt-get >/dev/null 2>&1; then
     echo "Debian/Ubuntu OS detected."
-    echo "Installing required packages..."
+    echo "Setting up container runtime..."
 
     export DEBIAN_FRONTEND=noninteractive
+
+    apt-get --quiet=2 --assume-yes update
+    install --mode=0755 --directory /etc/apt/keyrings
+    curl --fail --silent --show-error --location https://download.docker.com/linux/debian/gpg --output /etc/apt/keyrings/docker.asc
+    chmod u+r,g+r,o+r /etc/apt/keyrings/docker.asc
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     apt-get --quiet=2 --assume-yes update
 
@@ -30,11 +41,14 @@ if command -v apt-get >/dev/null 2>&1; then
         fi
     done
 
+    systemctl enable docker.service
+    systemctl enable containerd.service
+
     apt-get --quiet=2 --assume-yes autoremove
     apt-get --quiet=2 --assume-yes autoclean
     rm --recursive --force /tmp/* /var/tmp/* /var/cache/apt/* /var/lib/apt/lists/*
 
-    echo "Required packages installed successfully."
+    echo ""
 
 else
     echo "Unsupported OS detected. Please run this script on a Debian/Ubuntu-based system."
